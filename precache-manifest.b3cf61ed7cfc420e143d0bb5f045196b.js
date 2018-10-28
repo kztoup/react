7be7f3143a8b81de73cca3d81862453e4b851e7b
Konstantinos Ztoupis <kztoupis@tripsta.com>
2018-10-28 23:51:34 +0200

    Updates

---
+++ precache-manifest.b3cf61ed7cfc420e143d0bb5f045196b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bf2515a31126aef78a60",
+    "url": "/react/static/js/runtime~main.bf2515a3.js"
+  },
+  {
+    "revision": "d2102fa999dc6b6763ce",
+    "url": "/react/static/js/main.d2102fa9.chunk.js"
+  },
+  {
+    "revision": "37f90d57916b981a0157",
+    "url": "/react/static/js/1.37f90d57.chunk.js"
+  },
+  {
+    "revision": "d2102fa999dc6b6763ce",
+    "url": "/react/static/css/main.13205a66.chunk.css"
+  },
+  {
+    "revision": "57815115a1be3871f211886e394f6c61",
+    "url": "/react/index.html"
+  }
+];
